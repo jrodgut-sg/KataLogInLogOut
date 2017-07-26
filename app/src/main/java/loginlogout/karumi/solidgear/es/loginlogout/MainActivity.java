4925b11c12fbc79753222110e308baba1cbdb6e6
Javier Rodriguez <jrodriguez@apperian.com>
2017-07-26 13:33:39 +0200

    add main activity

---
+++ app/src/main/java/loginlogout/karumi/solidgear/es/loginlogout/MainActivity.java
@@ -0,0 +1,14 @@
+package loginlogout.karumi.solidgear.es.loginlogout;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
